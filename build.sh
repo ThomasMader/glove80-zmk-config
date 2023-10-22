b40693770ef4e0ddb5e79011fd80b07e102ba2ed
Thomas Mader <thomas.mader@gmail.com>
2023-10-22 15:41:52 +0200

    Add commit hash for zmk repo just to be safe

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 if [ ! -d "./src" ]; then
 	git clone https://github.com/moergo-sc/zmk.git src
+	cd src
+	# Don't think that is actually needed but let's keep it just to be safe.
+	#git reset --hard 279bc25747f038d7110c99d1903f2a8f482148b4
+	cd ..
 fi
 
 cp -f $(nix-build config -o combined --no-out-link)/glove80.uf2 .
