6045512823d19bec465049829c7de98bdf1be23d
Thomas Mader <thomas.mader@gmail.com>
2024-10-19 20:41:42 +0200

    Sync with Layout Editor state

---
+++ mybuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+tag=v24.02
+
+if [ -d "./src" ]; then
+  cd src
+  if [[ $(git tag) != $tag ]]; then
+    echo "Removing old src directory because it doesn't match the tag."
+    cd ..
+    rm -rf src
+  else
+    cd ..
+  fi
+fi
+
+if [ ! -d "./src" ]; then
+  git clone --depth 1 --branch $tag https://github.com/moergo-sc/zmk.git src
+fi
+
+cp -f $(nix-build config -o combined --no-out-link)/glove80.uf2 .
